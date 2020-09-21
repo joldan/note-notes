dbc59b2dce37635be00079d837e0c2a3a8e0c27a
Joaquin Oldan <joldan@gmail.com>
2020-09-21 08:07:17 -0300

    some visual content

--- controller/mainController.js
+++ controller/mainController.js
@@ -3,31 +3,31 @@ const Note = require('../models/note');
 
 exports.getIndex = (req, res, next) => {
     Note.findAll()
-    .then( notes => {
-        res.render('index', {
-            path : 'new-note',
-            notes: notes
-        })   
-    })
-    .catch( err => console.log(err))
+        .then(notes => {
+            res.render('index', {
+                path: 'new-note',
+                notes: notes
+            })
+        })
+        .catch(err => console.log(err))
 };
 
 exports.getEditNote = (req, res, next) => {
     const noteId = req.params.noteId;
-    Note.findByPk( noteId)
-    .then( note => {
-        res.render('edit-note', {
-            path : '/edit-note',
-            title: 'Edit Note',
-            note: note
-        });
-    })
-    .catch(err => console.log(err));
+    Note.findByPk(noteId)
+        .then(note => {
+            res.render('edit-note', {
+                path: '/edit-note',
+                title: 'Edit Note',
+                note: note
+            });
+        })
+        .catch(err => console.log(err));
 }
 
 exports.getNewNote = (req, res, next) => {
     res.render('new-note', {
-        path : 'new-note',
+        path: 'new-note',
         title: 'New Note',
     });
 }
@@ -35,14 +35,14 @@ exports.getNewNote = (req, res, next) => {
 exports.postNewNote = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
-    Note.create( {
-        title : title,
-        content : content
+    Note.create({
+        title: title,
+        content: content
     })
-    .then( result => {
-        res.redirect('/')
-    })
-    .catch(err => console.log(err));
+        .then(result => {
+            res.redirect('/')
+        })
+        .catch(err => console.log(err));
 }
 
 exports.postEditNote = (req, res, next) => {
@@ -51,11 +51,24 @@ exports.postEditNote = (req, res, next) => {
     const updatedContent = req.body.content;
 
     Note.findByPk(noteId)
-    .then(note => {
-        note.title = updatedTitle;
-        note.content = updatedContent;
-        return note.save()
-    })
-    .then(res.redirect('/'))
-    .catch( err => console.log(err))
+        .then(note => {
+            note.title = updatedTitle;
+            note.content = updatedContent;
+            return note.save()
+        })
+        .then(res.redirect('/'))
+        .catch(err => console.log(err))
+}
+
+exports.postDeleteNote = (req, res, next) => {
+    const noteId = req.body.noteId;
+
+    Note.findByPk(noteId)
+        .then( note => {
+            return note.destroy();
+        })
+        .then( result => {
+            res.redirect('/');
+        })
+        .catch(err => {console.log(err)})
 }
--- router/routes.js
+++ router/routes.js
@@ -13,6 +13,6 @@ router.post('/edit-note/:noteId', mainController.postEditNote)
 router.get('/new-note', mainController.getNewNote);
 router.post('/new-note', mainController.postNewNote);
 
-
+router.post('/delete-note', mainController.postDeleteNote);
 
 module.exports = router;
